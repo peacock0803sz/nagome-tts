4fd8c47f5871d0899ce182828dad1fd09a9b24d8
peacock <peacock0803sz@gmail.com>
2019-10-13 21:40:23 +0900

    :books: Create tag

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"context"
 	"log"
 	"os"
 
@@ -13,7 +13,9 @@ import (
 	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 )
 
-func ParseJson() (string) {
+const version = "0.1.0"
+
+func ParseJson() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	line := scanner.Text()
@@ -41,46 +43,46 @@ func ParseJson() (string) {
 }
 
 func speach(text string) {
-       // Instantiates a client.
-       ctx := context.Background()
+	// Instantiates a client.
+	ctx := context.Background()
 
-       client, err := texttospeech.NewClient(ctx)
-       if err != nil {
-               log.Fatal(err)
-       }
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-       // Perform the text-to-speech request on the text input with the selected
-       // voice parameters and audio file type.
-       req := texttospeechpb.SynthesizeSpeechRequest{
-               // Set the text input to be synthesized.
-               Input: &texttospeechpb.SynthesisInput{
-                       InputSource: &texttospeechpb.SynthesisInput_Text{Text: "こんにちは"},
-               },
-               // Build the voice request, select the language code ("en-US") and the SSML
-               // voice gender ("neutral").
-               Voice: &texttospeechpb.VoiceSelectionParams{
-                       LanguageCode: "ja-JP",
-                       SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
-               },
-               // Select the type of audio file you want returned.
-               AudioConfig: &texttospeechpb.AudioConfig{
-                       AudioEncoding: texttospeechpb.AudioEncoding_MP3,
-               },
-       }
-       resp, err := client.SynthesizeSpeech(ctx, &req)
-       if err != nil {
-               log.Fatal(err)
-       }
-       // The resp's AudioContent is binary.
-       filename := "output.mp3"
-       err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
-       if err != nil {
-               log.Fatal(err)
-       }
-       fmt.Printf("Audio content written to file: %v\n", filename)
+	// Perform the text-to-speech request on the text input with the selected
+	// voice parameters and audio file type.
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		// Set the text input to be synthesized.
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: text},
+		},
+		// Build the voice request, select the language code ("en-US") and the SSML
+		// voice gender ("neutral").
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "ja-JP",
+			SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+		},
+		// Select the type of audio file you want returned.
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// The resp's AudioContent is binary.
+	filename := "output.mp3"
+	err = ioutil.WriteFile(filename, resp.AudioContent, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Audio content written to file: %v\n", filename)
 }
 
-func main()  {
+func main() {
 	for {
 		text := ParseJson()
 		speach(text)
